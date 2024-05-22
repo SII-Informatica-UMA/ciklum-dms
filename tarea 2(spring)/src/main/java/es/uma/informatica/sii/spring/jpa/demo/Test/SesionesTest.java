46c0ecf517d86b56eb35b7fbea5ee17f3a9ed8fd
Santidiezzz <santidiez3@gmail.com>
2024-05-22 17:56:26 +0200

    Commit de los cambios

---
+++ tarea 2(spring)/src/main/java/es/uma/informatica/sii/spring/jpa/demo/Test/SesionesTest.java
@@ -0,0 +1,140 @@
+package es.uma.informatica.sii.spring.jpa.demo.Test;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriBuilderFactory;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DisplayName("En el servicio de sesiones")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+
+public class SesionesTest {
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Value(value = "${local.server.port}")
+    private int port;
+
+
+    @BeforeEach
+    public void initializeDatabase() {
+        grupoRepository.deleteAll();
+        alumnoRepository.deleteAll();
+    }
+
+    private URI uri(String scheme, String host, int port, String... paths) {
+        UriBuilderFactory ubf = new DefaultUriBuilderFactory();
+        UriBuilder ub = ubf.builder()
+            .scheme(scheme)
+            .host(host).port(port);
+        for (String path : paths) {
+            ub = ub.path(path);
+        }
+        return ub.build();
+    }
+    private URI uriParam(String scheme, String host, int port, String path,String param, Collection<String>values) {
+        UriBuilderFactory ubf = new DefaultUriBuilderFactory();
+        UriBuilder ub = ubf.builder()
+            .scheme(scheme)
+            .host(host).port(port)
+            .path(path).queryParam(param, values);
+        return ub.build();
+    }
+
+    private RequestEntity<Void> get(String scheme, String host, int port, String path) {
+        URI uri = uri(scheme, host, port, path);
+        var peticion = RequestEntity.get(uri)
+            .accept(MediaType.APPLICATION_JSON)
+            .build();
+        return peticion;
+    }
+    private RequestEntity<Void> getParam(String scheme, String host, int port, String path, int plan) {
+        URI uri = uriParam(scheme, host, port, path,"idSesion",plan);
+        var peticion = RequestEntity.get(uri)
+            .contentType(MediaType.APPLICATION_JSON);
+        return peticion.build();
+    }
+
+    private RequestEntity<Void> delete(String scheme, String host, int port, String path) {
+        URI uri = uri(scheme, host, port, path);
+        var peticion = RequestEntity.delete(uri)
+            .build();
+        return peticion;
+    }
+    private RequestEntity<Void> deleteParam(String scheme, String host, int port, String path,int plan) {
+        URI uri = uriParam(scheme, host, port, path,"idSesion",plan);
+        var peticion = RequestEntity.delete(uri)
+            .build();
+        return peticion;
+    }
+
+    private <T> RequestEntity<T> post(String scheme, String host, int port, String path, T object) {
+        URI uri = uri(scheme, host, port, path);
+        var peticion = RequestEntity.post(uri)
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(object);
+        return peticion;
+    }
+    private RequestEntity<Void> postParam(String scheme, String host, int port, String path, int plan) {
+        URI uri = uriParam(scheme, host, port, path,"idSesion",plan);
+        var peticion = RequestEntity.post(uri)
+            .contentType(MediaType.APPLICATION_JSON);
+        return peticion.build();
+    }
+
+    private <T> RequestEntity<T> put(String scheme, String host, int port, String path, T object) {
+        URI uri = uri(scheme, host, port, path);
+        var peticion = RequestEntity.put(uri)
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(object);
+        return peticion;
+    }
+    private RequestEntity<Void> putParam(String scheme, String host, int port, String path, int plan) {
+        URI uri = uriParam(scheme, host, port, path,"idSesion",plan);
+        var peticion = RequestEntity.put(uri)
+            .contentType(MediaType.APPLICATION_JSON);
+        return peticion.build();
+    }
+    @Nested
+    @DisplayName("cuando no hay datos")
+    public class SesionVacia{
+        @Test
+        @DisplayName("Devuelve la sesion vacia")
+        public void getSesionPlan(){
+            int plan = 1;
+            var peticion = getParam("http", "localhost", port, "/sesion", plan);
+            var respuesta = restTemplate.exchange(peticion, 
+            new ParameterizedTypeReference<Set<Sesion>>() {
+            });
+            assertThat(respuesta.getStatusCode().value()).isEqualTo(404);
+        }
+        @Test
+        @DisplayName("Devuelve error cuando añade una sesion a un plan especifico")
+        public void postSesionPlan(){
+            int plan = 1;
+            var peticion = postParam("http", "localhost", port, "/sesion", plan);
+            var respuesta = restTemplate.exchange(peticion, 
+            new ParameterizedTypeReference<Set<Sesion>>() {
+            });
+            assertThat(respuesta.getStatusCode().value()).isEqualTo(404);
+        }
+    }
+}
--- tarea 2(spring)/src/main/java/es/uma/informatica/sii/spring/jpa/demo/controllers/GestionUsuarios.java
+++ tarea 2(spring)/src/main/java/es/uma/informatica/sii/spring/jpa/demo/controllers/GestionUsuarios.java
@@ -6,7 +6,7 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-@RestC
+@Rest
 @RequestMapping("/sesion")
 @CrossOrigin()
 @Tag(name="Gestión de usuarios", description="Operaciones para la gestión de usuarios")
